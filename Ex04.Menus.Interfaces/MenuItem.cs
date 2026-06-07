6019198d6ad98ad821c73ac461eb1d025d8cd295
Eitan nathan Bar <eitanba@mta.ac.il>
2026-06-07 17:44:00 +0300

    Fixed naming

---
+++ Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MenuItem
+    {
+        public string m_Name;
+        public IMenuOptionsHandler m_Option;
+        List<MenuItem> m_SubMenu = new List<MenuItem>();
+
+        public MenuItem(string i_Name, IMenuOptionsHandler i_Option)
+        {
+            m_Name = i_Name;
+            m_Option = i_Option;
+        }
+    }
+}
