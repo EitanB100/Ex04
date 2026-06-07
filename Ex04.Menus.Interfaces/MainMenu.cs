97fa1d79556c22a8f5b4516a133358adc13bd07f
Eitan nathan Bar <eitanba@mta.ac.il>
2026-06-07 17:52:32 +0300

    Added main menu class

---
+++ Ex04.Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MainMenu
+    {
+        string m_Name;
+        List<MenuItem> m_MenuItems;
+
+        public MainMenu(string i_Name, List<MenuItem> i_MenuItems)
+        {
+            m_Name = i_Name;
+            m_MenuItems = i_MenuItems;
+        }
+
+        public void Show()
+        {
+            foreach (MenuItem item in m_MenuItems)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
--- Ex04.Menus.Interfaces/MenuItem.cs
+++ Ex04.Menus.Interfaces/MenuItem.cs
@@ -4,14 +4,38 @@ namespace Ex04.Menus.Interfaces
 {
     public class MenuItem
     {
-        public string m_Name;
-        public IMenuOptionsHandler m_Option;
-        List<MenuItem> m_SubMenu = new List<MenuItem>();
+        private string m_Name;
+        private IMenuOptionsHandler m_Option;
+        private List<MenuItem> m_SubMenu = new List<MenuItem>();
 
         public MenuItem(string i_Name, IMenuOptionsHandler i_Option)
         {
             m_Name = i_Name;
             m_Option = i_Option;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return m_Name;
+            }
+        }
+
+        public IMenuOptionsHandler Option
+        {
+            get
+            {
+                return m_Option;
+            }
+        }
+
+        public List<MenuItem> MenuItems
+        {
+            get
+            {
+                return m_SubMenu;
+            }
         }
     }
 }
