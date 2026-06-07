c4d8ff74aed627bcac6395d25101af2f010fb231
Eitan nathan Bar <eitanba@mta.ac.il>
2026-06-07 17:41:08 +0300

    Added menuOption class with ctor

---
+++ Ex04.Menus.Interfaces/MenuOption.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MenuOption
+    {
+        public string Name;
+        public IMenuOptionsHandler option;
+        List<MenuOption> subMenu;
+
+        public MenuOption(string i_Name, IMenuOptionsHandler i_Option)
+        {
+            Name = i_Name;
+            option = i_Option;
+        }
+    }
+}
