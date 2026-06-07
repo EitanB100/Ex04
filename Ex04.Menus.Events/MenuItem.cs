4af290749dcef041cde039c023e5f0396e0e8309
DeRoman10 <harel012345@gmail.com>
2026-06-07 18:45:24 +0300

    basic Menu Item

---
+++ Ex04.Menus.Events/MenuItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Events
+{
+    public class MenuItem
+    {
+        private string m_title;
+        private List<MenuItem> m_subItems = new List<MenuItem>();
+
+
+        public String Title
+        {
+            get
+            {
+                return m_title;
+            }
+            set
+            {
+                m_title = value;
+            }
+        }
+
+        public List<MenuItem> SubItems
+        {
+            get
+            {
+                return m_subItems;
+            }
+        }
+    }
+}
