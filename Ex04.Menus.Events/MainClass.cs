51832459b9508fcd01abaed0a431568390264382
DeRoman10 <harel012345@gmail.com>
2026-06-07 18:49:25 +0300

    make basic main class

---
+++ Ex04.Menus.Events/MainClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Events
+{
+    public class MainClass
+    {
+        private List<MenuItem> m_firstSubItems = new List<MenuItem>();
+        public List<MenuItem> SubItems
+        {
+            get
+            {
+                return m_firstSubItems;
+            }
+        }
+    }
+}
