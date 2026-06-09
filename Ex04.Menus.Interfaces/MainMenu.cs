f5efa3d855aae3a02742d7e473b7a10f605150a8
Eitan <goodsky2@gmail.com>
2026-06-09 14:43:30 +0300

    Interfaces done

--- Ex04.Menus.Interfaces/MainMenu.cs
+++ Ex04.Menus.Interfaces/MainMenu.cs
@@ -7,6 +7,8 @@ namespace Ex04.Menus.Interfaces
     {
         private string m_Name;
         private List<MenuItem> m_MenuItems = new List<MenuItem>();
+        private const int k_ExitChoice = 0;
+        private const int k_bufferSizeForSeparator = 6;
 
         public MainMenu(string i_Name)
         {
@@ -20,9 +22,67 @@ namespace Ex04.Menus.Interfaces
 
         public void Show()
         {
-            foreach (MenuItem item in m_MenuItems)
+            runCurrentMenu(m_Name, m_MenuItems, true);
+        }
+
+        private void runCurrentMenu(string i_CurrentMenuTitle, List<MenuItem> i_CurrentMenuItems, bool i_IsMainMenu)
+        {
+            bool askedToQuit = false;
+
+            while (!askedToQuit)
             {
-                Console.WriteLine(item.Name);
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("** {0} **", i_CurrentMenuTitle);
+
+                Console.ResetColor();
+
+                string separator = new string('-', i_CurrentMenuTitle.Length + k_bufferSizeForSeparator);
+
+                Console.WriteLine(separator);
+
+                for (int i = 1; i <= i_CurrentMenuItems.Count; i++)
+                {
+                    Console.WriteLine("{0}. {1}", i, i_CurrentMenuItems[i - 1].Name);
+                }
+
+                string exitOrBack = i_IsMainMenu ? "Exit" : "Back";
+                Console.WriteLine("0. {0}", exitOrBack);
+
+                Console.WriteLine("Please enter your choice (1-{0} or 0 to {1}):", i_CurrentMenuItems.Count, i_IsMainMenu ? "exit" : "go back");
+                Console.Write(">> ");
+
+                string userInput = Console.ReadLine();
+                int convertedChoice = 0;
+
+                while (!(int.TryParse(userInput, out convertedChoice)) || convertedChoice < 0 || convertedChoice > i_CurrentMenuItems.Count)
+                {
+                    Console.WriteLine("Invalid input. Please enter your choice (1-{0} or 0 to {1}):", i_CurrentMenuItems.Count, i_IsMainMenu ? "exit" : "go back");
+                    Console.Write(">>");
+
+                    userInput = Console.ReadLine();
+                }
+
+                if (convertedChoice == k_ExitChoice)
+                {
+                    askedToQuit = true;
+                }   
+                else
+                {
+                    if (i_CurrentMenuItems[convertedChoice - 1].MenuItems.Count > 0)
+                    {
+                        string subMenuTitle = i_CurrentMenuItems[convertedChoice - 1].Name;
+                        List<MenuItem> subMenuItems = i_CurrentMenuItems[convertedChoice - 1].MenuItems;
+
+                        Console.Clear();
+                        runCurrentMenu(subMenuTitle, subMenuItems, false);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        i_CurrentMenuItems[convertedChoice - 1].Option.InvokeMenuOptionSelected();
+                    }
+                }
             }
         }
     }
